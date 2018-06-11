228a60860cdc3ad3bdb18b561e8910e47bbd88bb
user2 <user2@accenture.com>
2018-06-11 15:31:16 +0530

    sop updated user2

--- src/com/app.java
+++ src/com/app.java
@@ -4,5 +4,5 @@ public class app {
 
 public static void main(String[] args) {
 System.out.println("Hello world");
-System.out.println("Hello World - Accenture-bdc3");
+System.out.println("Hello World - Accenture-Brisbane");
 }}
