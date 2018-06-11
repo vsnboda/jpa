c5a1ae0259133f6af86013f7b143504c69291849
user1 <user1@accenture.com>
2018-06-11 15:04:06 +0530

    resolving conflicts

--- src/com/app.java
+++ src/com/app.java
@@ -3,11 +3,7 @@ package com;
 public class app {
 
 public static void main(String[] args) {
-	System.out.println("Hello world");
-<<<<<<< HEAD
-	System.out.println("Hello World - Accenture-bdc3");
-=======
-	System.out.println("Hello World - Accenture-hdc2");
->>>>>>> refs/remotes/origin/master
-	System.out.println("Hello world - Accenture - 1km");
+System.out.println("Hello world");
+System.out.println("Hello World - Accenture-bdc3");
+System.out.println("Hello world - Accenture - 1km");
 }}
